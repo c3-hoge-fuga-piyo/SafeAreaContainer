0d95d666b125c3e1f86e9de83bc2e9883a362bad
Shimoda Takao <c3.hoge.fuga.piyo@gmail.com>
2021-04-29 22:26:44 +0900

    :new: first implements

---
+++ Assets/OhanaYa/UIElements.SafeAreaContainer/Runtime/SafeAreaContainer.cs
@@ -0,0 +1,55 @@
+#if (UNITY_2021_2_OR_NEWER && SAFE_AREA_CONTAINER_UI_ELEMENTS_MODULE_ENABLED) || SAFE_AREA_CONTAINER_UI_TOOLKIT_PACKAGE_INSTALLED
+#define SAFE_AREA_CONTAINER_UI_TOOLKIT_SUPPORT
+#endif
+
+#if SAFE_AREA_CONTAINER_UI_TOOLKIT_SUPPORT
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace OhanaYa.UIElements
+{
+    public sealed class SafeAreaContainer : VisualElement
+    {
+        #region Constructors
+        public SafeAreaContainer()
+        {
+            {
+                this.style.flexGrow = 1;
+                this.style.flexShrink = 1;
+            }
+
+            this.RegisterCallback<GeometryChangedEvent>(this.OnGeometryChanged);
+        }
+        #endregion
+
+        void OnGeometryChanged(GeometryChangedEvent e)
+        {
+            var panel = this.panel;
+
+#if UNITY_EDITOR
+            if (panel.contextType == ContextType.Editor)
+            {
+                //TODO: Is it possible to do the same in the editor?
+                return;
+            }
+#endif
+
+            var safeArea = Screen.safeArea;
+
+            var lt = RuntimePanelUtils.ScreenToPanel(panel, new Vector2(safeArea.xMin, Screen.height - safeArea.yMax));
+            var rb = RuntimePanelUtils.ScreenToPanel(panel, new Vector2(Screen.width - safeArea.xMax, safeArea.xMin));
+
+            {
+                this.style.paddingLeft = lt.x;
+                this.style.paddingTop = lt.y;
+                this.style.paddingRight = rb.x;
+                this.style.paddingBottom = rb.y;
+            }
+        }
+
+        #region VisualElement
+        public new class UxmlFactory : UxmlFactory<SafeAreaContainer, VisualElement.UxmlTraits> {}
+        #endregion
+    }
+}
+#endif
---
+++ README.md
@@ -0,0 +1,33 @@
+# SafeAreaContainer
+
+[![Releases](https://img.shields.io/github/release/c3-hoge-fuga-piyo/SafeAreaContainer.svg?style=flat-square)](https://github.com/c3-hoge-fuga-piyo/SafeAreaContainer/releases)
+
+Safe area support in UI Toolkit.
+
+## Usage
+
+### `OhanaYa.UIElements.SafeAreaContainer`
+
+```uxml
+<ui:UXML xmlns:ui="UnityEngine.UIElements" xmlns:uie="UnityEditor.UIElements" editor-extension-mode="False">
+    <OhanaYa.UIElements.SafeAreaContainer>
+        <ui:Label text="Hello, SafeAreaContainer!" />
+    </OhanaYa.UIElements.SafeAreaContainer>
+</ui:UXML>
+```
+
+## Requirements
+
+- Unity 2021.2 or later
+    - [`com.unity.modules.uielements`](https://docs.unity3d.com/2021.2/Documentation/Manual/com.unity.modules.uielements.html)
+- Unity 2021.1 or earlier
+    - [`com.unity.ui`](https://docs.unity3d.com/Packages/com.unity.ui@latest)
+
+## Installation
+
+- Unity Package Manager (Git URL) `https://github.com/c3-hoge-fuga-piyo/SafeAreaContainer.git?path=Assets/OhanaYa/UIElements.SafeAreaContainer`
+- [Asset Package](https://github.com/c3-hoge-fuga-piyo/SafeAreaContainer/releases)
+
+## License
+
+MIT License (see [LICENSE](LICENSE))
